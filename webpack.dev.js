34d8eb07caccf297c8f2cee383a86b177cfa134d
Nori Terada <noriterada19@gmail.com>
2024-01-09 03:11:22 -0700

    minifying css,js,html

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const common = require("./webpack.common");
 const {merge} = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 
 module.exports = merge(common, {
@@ -8,5 +9,23 @@ module.exports = merge(common, {
     output: {
         filename: "[name].main.js",
         path: path.resolve(__dirname, "dist")
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+        template: "./src/template.html",
+        })
+
+    ],
+    module:{
+        rules:[
+            {
+                test: /\.scss$/,
+                use: [
+                    "style-loader", 
+                    "css-loader",
+                    "sass-loader"
+                ]
+            }
+        ]
     }
 });
