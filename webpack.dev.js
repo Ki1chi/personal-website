25f2c658adaeea3d63039f93104d29c2c0deaa11
Nori Terada <noriterada19@gmail.com>
2024-01-09 02:18:35 -0700

    added multiple hashes

--- webpack.dev.js
+++ webpack.dev.js
@@ -6,8 +6,7 @@ const {merge} = require("webpack-merge");
 module.exports = merge(common, {
     mode: "development",
     output: {
-        filename: "main.js",
+        filename: "[name].main.js",
         path: path.resolve(__dirname, "dist")
-    },
-    plugins: [new CleanWebpackPlugin]
+    }
 });
