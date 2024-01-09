39b19eb32a6574b01339d2daba7c2e2aa64a0f42
Nori Terada <noriterada19@gmail.com>
2024-01-09 01:16:00 -0700

    added dev and production mode

--- webpack.common.js
+++ webpack.common.js
@@ -2,15 +2,13 @@ const path = require("path")
 var HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    mode: "development",
-    entry: ["./src/index.js"],
-    output: {
-        filename: "main.[contenthash].js",
-        path: path.resolve(__dirname,"dist")
-    },
-    plugins: [new HtmlWebpackPlugin({
+    entry: "./src/index.js",
+
+    plugins: [
+        new HtmlWebpackPlugin({
         template: "./src/template.html"
-    })],
+    })
+    ],
     module: {
         rules: [
             {
