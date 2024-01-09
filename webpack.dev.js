1f9491270e0e1a2d73e17246cc8f2c0453514a86
Nori Terada <noriterada19@gmail.com>
2024-01-09 01:54:09 -0700

    added html-loader, file-loader, and cleaned webpack

--- webpack.dev.js
+++ webpack.dev.js
@@ -2,10 +2,12 @@ const path = require("path");
 const common = require("./webpack.common");
 const {merge} = require("webpack-merge");
 
+
 module.exports = merge(common, {
     mode: "development",
     output: {
         filename: "main.js",
         path: path.resolve(__dirname, "dist")
     },
+    plugins: [new CleanWebpackPlugin]
 });
